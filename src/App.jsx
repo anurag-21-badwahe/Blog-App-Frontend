200b09582554702cf96139797897218b498d749a
anurag <anuragbadwahe@gmail.com>
2024-03-26 16:53:06 +0530

    Setup Sigin Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignIn from "./component/SignIn";
 
-import Navbar from './component/Navbar'
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/signin" element={<SignIn/>}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
